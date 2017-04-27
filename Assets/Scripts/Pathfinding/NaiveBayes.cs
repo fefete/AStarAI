5eef2aab7d99aba0ac5a5e062059c6d5896d1e26
Fernando Ferrando Terrádez <metrack92@gmail.com>
2017-04-27 15:30:30 +0100

    Naive bayes start

---
+++ Assets/Scripts/Pathfinding/NaiveBayes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class NaiveBayes : MonoBehaviour
+{
+
+    struct Data
+    {
+        int path { get; set; }
+        int hp_remaining { get; set; }
+        int agent_class { get; set; }
+        int has_shield { get; set; }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
